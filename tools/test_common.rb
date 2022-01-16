eda1a49fc435388af4536a6e8727ff138399cea2
Zachary Scott <zzakscott@gmail.com>
2022-01-16 09:35:53 +0900

    Store test results under node index too, and add artifacts for debugging
    I'm not sure why but it seems like only the last run is saved

--- tools/test_common.rb
+++ tools/test_common.rb
@@ -3,6 +3,11 @@
 if ENV["BUILDKITE"] || ENV["CIRCLECI"]
   require "minitest-ci"
 
-  job_id = ENV['BUILDKITE_JOB_ID'] || ENV['CIRCLE_BUILD_NUM']
+  job_id = ENV['BUILDKITE_JOB_ID']
+
+  if ENV["CIRCLECI"]
+    job_id = "#{ENV['CIRCLE_BUILD_NUM']}-#{ENV['CIRCLE_NODE_INDEX']}"
+  end
+
   Minitest::Ci.report_dir = File.join(__dir__, "../test-reports/#{job_id}")
 end
