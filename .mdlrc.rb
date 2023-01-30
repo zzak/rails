1309656cf7d305323d379a6dc97c21acf8de903a
zzak <zzakscott@gmail.com>
2023-01-31 07:53:10 +0900

    Lint: [action_cable, action_controller]_overview.md, disbale MD024

--- .mdlrc.rb
+++ .mdlrc.rb
@@ -7,6 +7,7 @@ exclude_rule "MD004"
 exclude_rule "MD005" # When using 2 spaces for MD007, this must be disabled
 exclude_rule "MD006"
 exclude_rule "MD014"
+exclude_rule "MD024"
 exclude_rule "MD026"
 exclude_rule "MD030"
 exclude_rule "MD033"
@@ -19,6 +20,6 @@ exclude_rule "MD041"
 #rule "MD004", style: :sublist
 rule "MD007", indent: 2
 rule "MD013", line_length: 2000, ignore_code_blocks: true
-rule "MD024", allow_different_nesting: true
+#rule "MD024", allow_different_nesting: true # This did not work as intended, see action_cable_overview.md
 rule "MD029", style: :ordered
 #rule "MD046", style: :consistent # default (:fenced)
