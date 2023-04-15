9f54eb829f10cab98494a48ad0712ab1b1ff1d64
zzak <zzakscott@gmail.com>
2023-04-15 19:06:58 +0900

    work

--- tools/test_common.rb
+++ tools/test_common.rb
@@ -3,5 +3,5 @@
 if ENV["BUILDKITE"]
   require "minitest-ci"
 
-  Minitest::Ci.report_dir = File.join(__dir__, "../test-reports/#{ENV['BUILDKITE_JOB_ID']}")
+  #Minitest::Ci.report_dir = File.join(__dir__, "../test-reports/#{ENV['BUILDKITE_JOB_ID']}")
 end
