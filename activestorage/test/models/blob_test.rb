a74e017d768a69f3723422ab27bf8d88712dd34f
zzak <zzakscott@gmail.com>
2024-04-23 07:41:39 +0900

    move blob urls test to urls directory

--- activestorage/test/models/blob_test.rb
+++ activestorage/test/models/blob_test.rb
@@ -205,65 +205,6 @@ class ActiveStorage::BlobTest < ActiveSupport::TestCase
     end
   end
 
-  test "URLs expiring in 5 minutes" do
-    blob = create_blob
-
-    freeze_time do
-      assert_equal expected_url_for(blob), blob.url
-      assert_equal expected_url_for(blob, disposition: :attachment), blob.url(disposition: :attachment)
-    end
-  end
-
-  test "URLs force content_type to binary and attachment as content disposition for content types served as binary" do
-    blob = create_blob(content_type: "text/html")
-
-    freeze_time do
-      assert_equal expected_url_for(blob, disposition: :attachment, content_type: "application/octet-stream"), blob.url
-      assert_equal expected_url_for(blob, disposition: :attachment, content_type: "application/octet-stream"), blob.url(disposition: :inline)
-    end
-  end
-
-  test "URLs force attachment as content disposition when the content type is not allowed inline" do
-    blob = create_blob(content_type: "application/zip")
-
-    freeze_time do
-      assert_equal expected_url_for(blob, disposition: :attachment, content_type: "application/zip"), blob.url
-      assert_equal expected_url_for(blob, disposition: :attachment, content_type: "application/zip"), blob.url(disposition: :inline)
-    end
-  end
-
-  test "URLs allow for custom filename" do
-    blob = create_blob(filename: "original.txt")
-    new_filename = ActiveStorage::Filename.new("new.txt")
-
-    freeze_time do
-      assert_equal expected_url_for(blob), blob.url
-      assert_equal expected_url_for(blob, filename: new_filename), blob.url(filename: new_filename)
-      assert_equal expected_url_for(blob, filename: new_filename), blob.url(filename: "new.txt")
-      assert_equal expected_url_for(blob, filename: blob.filename), blob.url(filename: nil)
-    end
-  end
-
-  test "URLs allow for custom options" do
-    blob = create_blob(filename: "original.txt")
-
-    arguments = [
-      blob.key
-    ]
-
-    kwargs = {
-      expires_in: ActiveStorage.service_urls_expire_in,
-      disposition: :attachment,
-      content_type: blob.content_type,
-      filename: blob.filename,
-      thumb_size: "300x300",
-      thumb_mode: "crop"
-    }
-    assert_called_with(blob.service, :url, arguments, **kwargs) do
-      blob.url(thumb_size: "300x300", thumb_mode: "crop")
-    end
-  end
-
   test "purge deletes file from external service" do
     blob = create_blob
 
@@ -355,14 +296,4 @@ class ActiveStorage::BlobTest < ActiveSupport::TestCase
       end
     end
   end
-
-  private
-    def expected_url_for(blob, disposition: :attachment, filename: nil, content_type: nil, service_name: :local)
-      filename ||= blob.filename
-      content_type ||= blob.content_type
-
-      key_params = { key: blob.key, disposition: ActionDispatch::Http::ContentDisposition.format(disposition: disposition, filename: filename.sanitized), content_type: content_type, service_name: service_name }
-
-      "https://example.com/rails/active_storage/disk/#{ActiveStorage.verifier.generate(key_params, expires_in: 5.minutes, purpose: :blob_key)}/#{filename}"
-    end
 end
