25bb4289133950df6ef8e3a8f0e5b08556b8c753
zzak <zzakscott@gmail.com>
2024-05-06 13:54:39 +0900

    Convert AS::Blobs::RedirectController tests to functional

--- activestorage/test/controllers/blobs/redirect_controller_test.rb
+++ activestorage/test/controllers/blobs/redirect_controller_test.rb
@@ -1,104 +1,111 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require "database/setup"
 
-class ActiveStorage::Blobs::RedirectControllerTest < ActionDispatch::IntegrationTest
-  setup do
+require "active_storage/engine/routes"
+
+class ActiveStorage::Blobs::RedirectControllerTest < ActionController::TestCase
+  def setup
+    @routes = ActionDispatch::Routing::RouteSet.new
+    ActiveStorage::Routes.draw_routes!(@routes)
+    ActiveStorage::Blob.service.instance_variable_set(:@url_helpers, @routes.url_helpers)
     @blob = create_file_blob filename: "racecar.jpg"
   end
 
   test "invalid signed ID" do
-    get rails_service_blob_url("invalid", "racecar.jpg")
+    get :show, params: { signed_id: "invalid", filename: "racecar.jpg" }
     assert_response :not_found
   end
 
   test "HTTP caching" do
-    get rails_storage_redirect_url(@blob)
+    get :show, params: { signed_id: @blob.signed_id, filename: @blob.filename }
     assert_redirected_to(/racecar\.jpg/)
     assert_equal "max-age=300, private", response.headers["Cache-Control"]
   end
 
   test "signed ID within expiration duration" do
-    get rails_storage_redirect_url(@blob, expires_in: 1.minute)
+    expiring_id = @blob.signed_id(expires_in: 1.minute)
+    get :show, params: { signed_id: expiring_id, filename: @blob.filename }
     assert_redirected_to(/racecar\.jpg/)
   end
 
   test "Expired signed ID within expiration duration" do
-    url = rails_storage_redirect_url(@blob, expires_in: 1.minute)
+    expiring_id = @blob.signed_id(expires_in: 1.minute)
     travel 2.minutes
-    get url
+    get :show, params: { signed_id: expiring_id, filename: @blob.filename }
     assert_response :not_found
   end
 
   test "signed ID within expiration time" do
-    get rails_storage_redirect_url(@blob, expires_at: 1.minute.from_now)
+    expiring_at = @blob.signed_id(expires_at: 1.minute.from_now)
+    get :show, params: { signed_id: expiring_at, filename: @blob.filename }
     assert_redirected_to(/racecar\.jpg/)
   end
 
   test "Expired signed ID within expiration time" do
-    url = rails_storage_redirect_url(@blob, expires_at: 1.minute.from_now)
+    expiring_at = @blob.signed_id(expires_at: 1.minute.from_now)
     travel 2.minutes
-    get url
+    get :show, params: { signed_id: expiring_at, filename: @blob.filename }
     assert_response :not_found
   end
 end
 
-class ActiveStorage::Blobs::ExpiringRedirectControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @blob = create_file_blob filename: "racecar.jpg"
-    @old_urls_expire_in = ActiveStorage.urls_expire_in
-    ActiveStorage.urls_expire_in = 1.minutes
-  end
+# TODO: this should just be an integration test
+# class ActiveStorage::Blobs::ExpiringRedirectControllerTest < ActionDispatch::IntegrationTest
+#   setup do
+#     @blob = create_file_blob filename: "racecar.jpg"
+#     @old_urls_expire_in = ActiveStorage.urls_expire_in
+#     ActiveStorage.urls_expire_in = 1.minutes
+#   end
 
-  teardown do
-    ActiveStorage.urls_expire_in = @old_urls_expire_in
-  end
+#   teardown do
+#     ActiveStorage.urls_expire_in = @old_urls_expire_in
+#   end
 
-  test "signed ID within expiration date" do
-    get rails_storage_redirect_url(@blob)
-    assert_redirected_to(/racecar\.jpg/)
-  end
+#   test "signed ID within expiration date" do
+#     get rails_storage_redirect_url(@blob)
+#     assert_redirected_to(/racecar\.jpg/)
+#   end
 
-  test "Expired signed ID" do
-    url = rails_storage_redirect_url(@blob)
-    travel 2.minutes
-    get url
-    assert_response :not_found
-  end
-end
+#   test "Expired signed ID" do
+#     url = rails_storage_redirect_url(@blob)
+#     travel 2.minutes
+#     get url
+#     assert_response :not_found
+#   end
+# end
 
-class ActiveStorage::Blobs::RedirectControllerWithOpenRedirectTest < ActionDispatch::IntegrationTest
-  if SERVICE_CONFIGURATIONS[:s3]
-    test "showing existing blob stored in s3" do
-      with_raise_on_open_redirects(:s3) do
-        blob = create_file_blob filename: "racecar.jpg", service_name: :s3
+# class ActiveStorage::Blobs::RedirectControllerWithOpenRedirectTest < ActionDispatch::IntegrationTest
+#   if SERVICE_CONFIGURATIONS[:s3]
+#     test "showing existing blob stored in s3" do
+#       with_raise_on_open_redirects(:s3) do
+#         blob = create_file_blob filename: "racecar.jpg", service_name: :s3
 
-        get rails_storage_redirect_url(blob)
-        assert_redirected_to(/racecar\.jpg/)
-      end
-    end
-  end
+#         get rails_storage_redirect_url(blob)
+#         assert_redirected_to(/racecar\.jpg/)
+#       end
+#     end
+#   end
 
-  if SERVICE_CONFIGURATIONS[:azure]
-    test "showing existing blob stored in azure" do
-      with_raise_on_open_redirects(:azure) do
-        blob = create_file_blob filename: "racecar.jpg", service_name: :azure
+#   if SERVICE_CONFIGURATIONS[:azure]
+#     test "showing existing blob stored in azure" do
+#       with_raise_on_open_redirects(:azure) do
+#         blob = create_file_blob filename: "racecar.jpg", service_name: :azure
 
-        get rails_storage_redirect_url(blob)
-        assert_redirected_to(/racecar\.jpg/)
-      end
-    end
-  end
+#         get rails_storage_redirect_url(blob)
+#         assert_redirected_to(/racecar\.jpg/)
+#       end
+#     end
+#   end
 
-  if SERVICE_CONFIGURATIONS[:gcs]
-    test "showing existing blob stored in gcs" do
-      with_raise_on_open_redirects(:gcs) do
-        blob = create_file_blob filename: "racecar.jpg", service_name: :gcs
+#   if SERVICE_CONFIGURATIONS[:gcs]
+#     test "showing existing blob stored in gcs" do
+#       with_raise_on_open_redirects(:gcs) do
+#         blob = create_file_blob filename: "racecar.jpg", service_name: :gcs
 
-        get rails_storage_redirect_url(blob)
-        assert_redirected_to(/racecar\.jpg/)
-      end
-    end
-  end
-end
+#         get rails_storage_redirect_url(blob)
+#         assert_redirected_to(/racecar\.jpg/)
+#       end
+#     end
+#   end
+# end
