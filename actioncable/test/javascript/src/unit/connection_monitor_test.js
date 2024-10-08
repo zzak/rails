716b6b28e3fd15dd3207b85946bd8da66fdf6aa9
zzak <zzakscott@gmail.com>
2024-10-08 09:26:28 +0900

    mocha

--- actioncable/test/javascript/src/unit/connection_monitor_test.js
+++ actioncable/test/javascript/src/unit/connection_monitor_test.js
@@ -1,68 +1,81 @@
-import * as ActionCable from "../../../../app/javascript/action_cable/index"
+import * as ActionCable from "../../../../app/javascript/action_cable/index";
 
-const {module, test} = QUnit
+describe("ActionCable.ConnectionMonitor", function() {
+  let monitor;
 
-module("ActionCable.ConnectionMonitor", hooks => {
-  let monitor
-  hooks.beforeEach(() => monitor = new ActionCable.ConnectionMonitor({}))
+  beforeEach(function() {
+    monitor = new ActionCable.ConnectionMonitor({});
+  });
 
-  module("#getPollInterval", hooks => {
-    hooks.beforeEach(() => Math._random = Math.random)
-    hooks.afterEach(() => Math.random = Math._random)
+  describe("#getPollInterval", function() {
+    const { staleThreshold, reconnectionBackoffRate } = ActionCable.ConnectionMonitor;
+    const backoffFactor = 1 + reconnectionBackoffRate;
+    const ms = 1000;
 
-    const { staleThreshold, reconnectionBackoffRate } = ActionCable.ConnectionMonitor
-    const backoffFactor = 1 + reconnectionBackoffRate
-    const ms = 1000
+    beforeEach(function() {
+      this._originalMathRandom = Math.random;
+    });
 
-    test("uses exponential backoff", assert => {
-      Math.random = () => 0
+    afterEach(function() {
+      Math.random = this._originalMathRandom;
+    });
 
-      monitor.reconnectAttempts = 0
-      assert.equal(monitor.getPollInterval(), staleThreshold * ms)
+    it("uses exponential backoff", function() {
+      Math.random = () => 0;
 
-      monitor.reconnectAttempts = 1
-      assert.equal(monitor.getPollInterval(), staleThreshold * backoffFactor * ms)
+      monitor.reconnectAttempts = 0;
+      assert.equal(monitor.getPollInterval(), staleThreshold * ms);
 
-      monitor.reconnectAttempts = 2
-      assert.equal(monitor.getPollInterval(), staleThreshold * backoffFactor * backoffFactor * ms)
-    })
+      monitor.reconnectAttempts = 1;
+      assert.equal(monitor.getPollInterval(), staleThreshold * backoffFactor * ms);
 
-    test("caps exponential backoff after some number of reconnection attempts", assert => {
-      Math.random = () => 0
-      monitor.reconnectAttempts = 42
-      const cappedPollInterval = monitor.getPollInterval()
+      monitor.reconnectAttempts = 2;
+      assert.equal(monitor.getPollInterval(), staleThreshold * backoffFactor * backoffFactor * ms);
+    });
 
-      monitor.reconnectAttempts = 9001
-      assert.equal(monitor.getPollInterval(), cappedPollInterval)
-    })
+    it("caps exponential backoff after some number of reconnection attempts", function() {
+      Math.random = () => 0;
+      monitor.reconnectAttempts = 42;
+      const cappedPollInterval = monitor.getPollInterval();
 
-    test("uses 100% jitter when 0 reconnection attempts", assert => {
-      Math.random = () => 0
-      assert.equal(monitor.getPollInterval(), staleThreshold * ms)
+      monitor.reconnectAttempts = 9001;
+      assert.equal(monitor.getPollInterval(), cappedPollInterval);
+    });
 
-      Math.random = () => 0.5
-      assert.equal(monitor.getPollInterval(), staleThreshold * 1.5 * ms)
-    })
+    it("uses 100% jitter when 0 reconnection attempts", function() {
+      Math.random = () => 0;
+      assert.equal(monitor.getPollInterval(), staleThreshold * ms);
 
-    test("uses reconnectionBackoffRate for jitter when >0 reconnection attempts", assert => {
-      monitor.reconnectAttempts = 1
+      Math.random = () => 0.5;
+      assert.equal(monitor.getPollInterval(), staleThreshold * 1.5 * ms);
+    });
 
-      Math.random = () => 0.25
-      assert.equal(monitor.getPollInterval(), staleThreshold * backoffFactor * (1 + reconnectionBackoffRate * 0.25) * ms)
+    it("uses reconnectionBackoffRate for jitter when >0 reconnection attempts", function() {
+      monitor.reconnectAttempts = 1;
 
-      Math.random = () => 0.5
-      assert.equal(monitor.getPollInterval(), staleThreshold * backoffFactor * (1 + reconnectionBackoffRate * 0.5) * ms)
-    })
+      Math.random = () => 0.25;
+      assert.equal(
+        monitor.getPollInterval(),
+        staleThreshold * backoffFactor * (1 + reconnectionBackoffRate * 0.25) * ms
+      );
 
-    test("applies jitter after capped exponential backoff", assert => {
-      monitor.reconnectAttempts = 9001
+      Math.random = () => 0.5;
+      assert.equal(
+        monitor.getPollInterval(),
+        staleThreshold * backoffFactor * (1 + reconnectionBackoffRate * 0.5) * ms
+      );
+    });
 
-      Math.random = () => 0
-      const withoutJitter = monitor.getPollInterval()
-      Math.random = () => 0.5
-      const withJitter = monitor.getPollInterval()
+    it("applies jitter after capped exponential backoff", function() {
+      monitor.reconnectAttempts = 9001;
 
-      assert.ok(withJitter > withoutJitter)
-    })
-  })
-})
+      Math.random = () => 0;
+      const withoutJitter = monitor.getPollInterval();
+
+      Math.random = () => 0.5;
+      const withJitter = monitor.getPollInterval();
+
+      assert.isAbove(withJitter, withoutJitter);
+    });
+  });
+});
--- actioncable/test/javascript/src/unit/connection_test.js
+++ actioncable/test/javascript/src/unit/connection_test.js
@@ -1,28 +1,30 @@
-import * as ActionCable from "../../../../app/javascript/action_cable/index"
+import * as ActionCable from "../../../../app/javascript/action_cable/index";
 
-const {module, test} = QUnit
+describe("ActionCable.Connection", function() {
+  describe("#getState", function() {
+    it("uses the configured WebSocket adapter", function() {
+      ActionCable.adapters.WebSocket = { foo: 1, BAR: "42" };
+      const connection = new ActionCable.Connection({});
+      connection.webSocket = {};
 
-module("ActionCable.Connection", () => {
-  module("#getState", () => {
-    test("uses the configured WebSocket adapter", assert => {
-      ActionCable.adapters.WebSocket = { foo: 1, BAR: "42" }
-      const connection = new ActionCable.Connection({})
-      connection.webSocket = {}
-      connection.webSocket.readyState = 1
-      assert.equal(connection.getState(), "foo")
-      connection.webSocket.readyState = "42"
-      assert.equal(connection.getState(), "bar")
-    })
-  })
+      connection.webSocket.readyState = 1;
+      assert.equal(connection.getState(), "foo");
 
-  module("#open", () => {
-    test("uses the configured WebSocket adapter", assert => {
-      const FakeWebSocket = function() {}
-      ActionCable.adapters.WebSocket = FakeWebSocket
-      const connection = new ActionCable.Connection({})
-      connection.monitor = { start() {} }
-      connection.open()
-      assert.equal(connection.webSocket instanceof FakeWebSocket, true)
-    })
-  })
-})
+      connection.webSocket.readyState = "42";
+      assert.equal(connection.getState(), "bar");
+    });
+  });
+
+  describe("#open", function() {
+    it("uses the configured WebSocket adapter", function() {
+      const FakeWebSocket = function() {};
+      ActionCable.adapters.WebSocket = FakeWebSocket;
+
+      const connection = new ActionCable.Connection({});
+      connection.monitor = { start() {} };
+      connection.open();
+
+      assert.equal(connection.webSocket instanceof FakeWebSocket, true);
+    });
+  });
+});
